6690f309393f0226fd38072fa9ccbfdef1c23f11
Fidel Dubon <fideldubon@gmail.com>
2018-04-20 23:17:22 -0500

    Did more coding

---
+++ assets/javascript/app.js
@@ -0,0 +1,83 @@
+
+
+
+var timer = 30;
+var intervalId;
+var correctAns = 0;
+var wrongAns = 0;
+var unanswered = 0;
+
+$(".questions").hide();
+
+// // function myFunction() {
+// //     var x = document.getElementById("myDIV");
+// //     if (x.style.display === "none") {
+// //         x.style.display = "block";
+// //     } else {
+// //         x.style.display = "none";
+// //     }
+
+
+// // $("#myDIV").hide();
+// // $(".myDIV").hide();
+// // $(".myDIV").hide();
+// // $("#cDTimer").hide();
+// // $("#show-number").hide();
+// // $("#submit-button").hide();
+// // $(".results").hide();
+
+// // $("#start-game").on("click", function () {
+// //     $("#start-game").hide();
+// //     $("#myDIV").show();
+// //     $("#timer").show();
+// //     $("#submit-answer").show();
+
+// //     if (intervalId) {
+// //         clearInterval(intervalId);
+// //     }
+// //     intervalId = setInterval(decrement, 1000);
+// //     function decrement() {
+
+// //         number--;
+
+// //         $("#timer").html("<h2>Time remaining: " + number + " seconds.</h2>");
+
+// //         if (number === 0) {
+// //             clearInterval(intervalId);
+// //         }
+
+// //     }
+
+
+// //     $("#dispBox").on("start-game", function () {
+// //         event.preventDefault();
+// //         $("#myDIV").hide();
+// //         $(".results").show();
+// //         $("#timer").hide();
+// //     })})
+
+// //     //     var $firstCorrAns = $(".first-group-answers");
+// // //     var answer = $firstCorrAns.val();
+
+// // //     if (answer === "firstAnswer2") {
+// // //         correctAns++
+// // //     } else {
+// // //         incorrectAns++
+// // //     };
+
+// // //     if (!answer) {
+// // //         unanswered++
+// // //     };
+
+// // //     var html =
+// // //         "<p>Correct answers: " + correctAns + "</p>" +
+// // //         "<p>Incorrect answers: " + incorrectAns + "</p>" +
+// // //         "<p>Unanswered: " + unanswered + "</p>";
+
+
+// // //     document.querySelector(".results").innerHTML = html;
+
+
+// // //     // $(".results").empty();
+
+// }
